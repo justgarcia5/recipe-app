1c051f2dde504b0f27bde4fd5ab0b1c7e5c00277
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-05-27 15:15:37 -0700

    made css changes

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='App'>
+      <br />
       <h1>About</h1>
     </div>
   )
--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,11 @@ import RecipeApi from './components/Recipe';
 function Home() {
   return (
     <div className="App">
-      <br />
+      <br/>
       <h1>RecipeApp</h1>
-      <div>
-        <DateApi />
-      </div>
+      <DateApi />
       <RecipeApi />
+      <br/>
     </div>
   );
 }
