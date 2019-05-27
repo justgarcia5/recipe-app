29cdd5bd9af02ab616a009d334ed588e62913374
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-05-27 00:01:23 -0700

    add recipe API/neen to finish view

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 
 import DateApi from './components/DateApi'
+import RecipeApi from './components/Recipe';
 
 function Home() {
   return (
     <div className="App">
+      <br />
+      <h1>RecipeApp</h1>
       <div>
         <DateApi />
       </div>
+      <RecipeApi />
     </div>
   );
 }
