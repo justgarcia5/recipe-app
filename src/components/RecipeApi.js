749cd507333c2a685bd31fc4dd2e1eee40009c37
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-05-30 11:23:20 -0700

    created a recipe card comp and recipe detail comp/need to finish recipe detail comp

--- src/components/RecipeApi.js
+++ src/components/RecipeApi.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import LandingImage from './RecipesLanding'
+import RecipeLanding from './RecipesLanding'
 
 const APP_KEY = 'b553d12844b50e38e7f0426de87800e2'
 const APP_ID = '61f1760b'
@@ -13,13 +13,13 @@ class RecipeApi extends React.Component {
     errors: null,
   }
 
-  componentDidMount = () => {
-    fetch(`https://api.edamam.com/search?q=Popular&app_id=${APP_ID}&app_key=${APP_KEY}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ pop: data })
-      })
-  }
+  // componentDidMount = () => {
+  //   fetch(`https://api.edamam.com/search?q=Popular&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       this.setState({ pop: data })
+  //     })
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault()
@@ -31,7 +31,6 @@ class RecipeApi extends React.Component {
     }).then(response => response.json())
       .then(data => this.setState({ data: data, responseOk: true }))
       .catch(errors => {
-        console.log(errors)
         this.setState({ errors: errors })
       })
   }
@@ -42,25 +41,26 @@ class RecipeApi extends React.Component {
   }
 
   render() {
-    let { errors } = this.state
+    let { errors } = this.props
 
     return (
       <div className='recipe-input'>
         <form onSubmit={this.handleSubmit} className='input-form'>
           <input
             className='input'
+            placeholder='Chicken, Deserts, Healthy...'
             type='text'
             name='search'
             onChange={this.inputHandler}
             value={this.state.search}
           />
-          <button type='submit'>Search</button>
+          <button type='submit' className='input-form-button'>Search</button>
         </form>
-        {errors}
-        <LandingImage
+        <RecipeLanding
           data={this.state.data}
           response={this.state.responseOk}
           pop={this.state.pop}
+          errors={errors}
         />
       </div>
     )
