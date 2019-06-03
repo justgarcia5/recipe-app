ca0a7d976e1c18cfbd5dfece75918eedf91c1d61
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-06-03 16:21:53 -0700

    css changes

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -13,6 +13,7 @@ const RecipeCards = props => {
               <Link to={`/recipe-detail/${index}/${hit.recipe.label}`} data={props.hits}>
                 <ul>
                   <li><img src={hit.recipe.image} alt='recipe-pic' className='recipe-image' /></li>
+                  <hr/>
                   <li className='recipe-label'>{hit.recipe.label}</li>
                   {/* <li>{hit.recipe.source}</li> */}
                   {
@@ -35,6 +36,7 @@ const RecipeCards = props => {
               <Link to={`/recipe-detail/${index}/${recipe.label}`} data={props.hits}>
                 <ul>
                   <li><img src={recipe.image} alt='recipe-pic' className='recipe-image' /></li>
+                  <hr/>
                   <li className='recipe-label'>{recipe.label}</li>
                   {/* <li>{recipe.source}</li> */}
                   {
