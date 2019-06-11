a7e91c86f357d5a94ae1a090243e22255ed8935b
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-06-11 16:22:14 -0700

    made css changes/fixed error handle and css

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -12,8 +12,8 @@ const NavBar = props => {
             <div className='navbar-right'>
               <li><b>Welcome <i>{current_user.username}</i></b></li>
               <li><a href='/members/favorites'><b>Favorites</b></a></li>
-              <li><a href="/users/sign_out" rel="nofollow" data-method="delete"><b>Sign Out</b></a></li>
-              <li><a href='/users/edit'><b>Edit User</b></a></li>
+              <li><a href="/users/sign_out" rel="nofollow" data-method="delete"><b>SignOut</b></a></li>
+              <li><a href='/users/edit'><b>EditUser</b></a></li>
             </div>
           </React.Fragment>
         }
@@ -22,7 +22,7 @@ const NavBar = props => {
             <a href='/' className='NavBar-home'> <li><b>RecipeApp</b></li></a>
             <div className='navbar-right'>
               <a href="/users/sign_in"><li><b>Login</b></li></a>
-              <a href="/users/sign_up"><li><b>Sign up</b></li></a>
+              <a href="/users/sign_up"><li><b>SignUp</b></li></a>
             </div>
           </React.Fragment>
         }
