7e81d7ee2b78a8671134e2291b5dc9844e7f2a68
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-06-12 13:55:20 -0700

    trying to config devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'yoursite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'blooming-brushlands-16807.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
