c15338f5e5878e7ae9053e864b874b8f2876f804
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-06-12 18:02:41 -0700

    heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ‘localhost:3000’ }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => “utf-8”
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: “edit_your_email_here@gmail.com” }
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 2525,
-    domain: "gmail.com",
-    authentication: "plain",
+    address: “smtp.gmail.com”,
+    port: 587,
+    authentication: “plain”,
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV[‘GMAIL_USERNAME’],
+    password: ENV[‘GMAIL_PASSWORD’],
+    openssl_verify_mode: ‘none’
   }
+  config.action_mailer.perform_caching = false
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,15 +6,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+    address: ‘smtp.sendgrid.net’,
+    port: “25”,
+    domain: ‘heroku.com’,
+    user_name: ENV[“SENDGRID_USERNAME”],
+    password: ENV[“SENDGRID_PASSWORD”],
+    authentication: ‘plain’,
+    enable_starttls_auto: true
   }
+
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
