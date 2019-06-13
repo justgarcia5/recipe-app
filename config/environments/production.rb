d4c7de32e1e94c689c6168680418b9aa26b832d6
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-06-12 18:07:30 -0700

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { :host => 'blooming-brushlands-16807.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
 
   ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: ‘smtp.sendgrid.net’,
     port: “25”,
     domain: ‘heroku.com’,
