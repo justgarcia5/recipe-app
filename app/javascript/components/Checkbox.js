f0402c80d26ccc4c016feeaf89154e95cad09d05
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-08-05 23:11:13 -0700

    added functioning checkbox for favorites / Still need to finish PUT and recipe database

--- app/javascript/components/Checkbox.js
+++ app/javascript/components/Checkbox.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-const APP_KEY = 'b553d12844b50e38e7f0426de87800e2'
-const APP_ID = '61f1760b'
-
 class Checkbox extends React.Component {
   state = {
     checked: false
@@ -11,15 +8,7 @@ class Checkbox extends React.Component {
   checkBoxHandler = (e) => {
     let checked = e.target.checked
     if(checked === true) {
-      fetch(`https://api.edamam.com/search?q=${this.props.params.label}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-      .then(response => response.json())
-      .then((data) => {
-        let recipes = data.hits.map((hit) => hit.recipe)
-        let filteredFavorites = recipes.filter((recipe, index) => recipe.label === this.props.params.label && index === 0)
-      console.log(filteredFavorites)
-      this.setState({ checked: checked })
-
-      })
+      console.log(this.props.recipe)
     } else {
       console.log("false")
       this.setState({ checked: checked })
