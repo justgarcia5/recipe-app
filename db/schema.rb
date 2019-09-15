8d2642d9309094755635382104f02e0f94f6520f
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-09-15 00:13:17 -0700

    user can add comment to blog not finished. comment components built with react hooks

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_002330) do
+ActiveRecord::Schema.define(version: 2019_09_15_021857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "username"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.text "text"
@@ -61,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_09_13_002330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "posts", "users"
   add_foreign_key "recipes", "users"
