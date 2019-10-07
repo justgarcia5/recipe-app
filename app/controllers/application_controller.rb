4ad64dc5a44cce59910e5b5670ae9b35ac1a1db9
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-10-07 15:04:35 -0700

    added active storage with amazon S3 and cofig

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   protected
 
   def configure_permitted_parameters
 
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :avatar, :avatar_cache)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :avatar, :avatar_cache, :remove_avatar)}
 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :recipes, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_one_attached :avatar
 
 end
