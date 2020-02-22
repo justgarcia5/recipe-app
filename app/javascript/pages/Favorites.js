89bc6be44d6f237af7168d8bd42c3eb37355e4ee
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-02-22 12:12:57 -0800

    updated favorites.js component

--- app/javascript/pages/Favorites.js
+++ app/javascript/pages/Favorites.js
@@ -18,19 +18,19 @@ const Favorites = () => {
     <div className='favorites-div'>
       <h1 className='favorites-title'>Favorites</h1>
       <div className='search-result-container'>
-      {recipes.map((recipe, index) => {
-        return (
-          <div key={index} className='recipe-card'>
-            <Link to={`/members/favorites/detail/${recipe.id}`} >
-              <ul>
-                <li><img src={recipe.image} alt='recipe-pic' className='recipe-image'/></li>
-                <hr/>
-                <li className='recipe-label'>{recipe.label}</li>
-              </ul>
-            </Link>
-          </div>
-        )})
-      }
+        {recipes.map((recipe, index) => {
+          return (
+            <div key={index} className='recipe-card'>
+              <Link to={`/members/favorites/detail/${recipe.id}`} >
+                <ul>
+                  <li><img src={recipe.image} alt='recipe-pic' className='recipe-image'/></li>
+                  <hr/>
+                  <li className='recipe-label'>{recipe.label}</li>
+                </ul>
+              </Link>
+            </div>
+          )})
+        }
       </div>
     </div>
   )
